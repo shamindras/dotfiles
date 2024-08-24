942cfe705b328eaffd33ac3b98ab7f51fda8ca98
Shamindra Shrotriya <shamindra.shrotriya@gmail.com>
2024-08-23 21:49:33 -0500

    refactor: move z1-history into separate module

--- config/zsh/conf.d/06-z1-history.zsh
+++ config/zsh/conf.d/06-z1-history.zsh
@@ -1,54 +1,31 @@
 #!/bin/zsh
 
 # ------------------------------------------------------------------------------
-# region: History
+# region: Set options, variables, and aliases for Zsh history
 # ------------------------------------------------------------------------------
 
-() {
-  # Set Zsh options related to history.
+# z1_history: 
+function z1_history {
   setopt bang_hist               # Treat the '!' character specially during expansion.
   setopt extended_history        # Write the history file in the ':start:elapsed;command' format.
   setopt hist_expire_dups_first  # Expire a duplicate event first when trimming history.
-  setopt hist_find_no_dups       # Do not display a previously found event.
-  setopt hist_ignore_all_dups    # Delete an old recorded event if a new event is a duplicate.
+  setopt hist_find_no_dups       # When using find, only display a command once.
   setopt hist_ignore_dups        # Do not record an event that was just recorded again.
   setopt hist_ignore_space       # Do not record an event starting with a space.
-  setopt hist_reduce_blanks      # Remove extra blanks from commands added to the history list.
-  setopt hist_save_no_dups       # Do not write a duplicate event to the history file.
   setopt hist_verify             # Do not execute immediately upon history expansion.
   setopt inc_append_history      # Write to the history file immediately, not when the shell exits.
-  setopt NO_hist_beep            # Don't beep when accessing non-existent history.
-  setopt NO_share_history        # Don't share history between all sessions.
+  setopt NO_share_history        # Do not share history between all sessions.
+  setopt NO_hist_beep            # Do not beep when accessing non-existent history.
 
-  # Set the path to the default history file.
-  if zstyle -s ':zephyr:plugin:history' histfile 'HISTFILE'; then
-    # Make sure the user didn't store a HISTFILE with a leading '~'.
-    HISTFILE="${~HISTFILE}"
-  else
-    if zstyle -T ':zephyr:plugin:history' use-xdg-basedirs; then
-      : ${__zsh_user_data_dir:=${XDG_DATA_HOME:-$HOME/.local/share}/zsh}
-      HISTFILE="${__zsh_user_data_dir}/zsh_history"
-    else
-      HISTFILE="${ZDOTDIR:-$HOME}/.zsh_history"
-    fi
-  fi
+  # Set the path to the history file.
+  zstyle -s ':z1:history' histfile 'HISTFILE' || HISTFILE="$__zsh_user_data_dir/zsh_history"
+  # Set the maximum number of events to save in the internal history.
+  zstyle -s ':z1:history' histsize 'HISTSIZE' || HISTSIZE=20000
+  # Set the maximum number of events to save in the history file.
+  zstyle -s ':z1:history' savehist 'SAVEHIST' || SAVEHIST=100000
 
-  # Make sure the HISTFILE's directory exists.
-  [[ -d "${HISTFILE:h}" ]] || mkdir -p "${HISTFILE:h}"
-
-  # Set history file size.
-  zstyle -s ':zephyr:plugin:history' savehist 'SAVEHIST' \
-    || SAVEHIST=100000
-
-  # Set session history size.
-  zstyle -s ':zephyr:plugin:history' histsize 'HISTSIZE' \
-    || HISTSIZE=20000
-
-  # Set history aliases.
-  if ! zstyle -t ':zephyr:plugin:history:alias' skip; then
-    alias hist='fc -li'
-    alias history-stat="history 0 | awk '{print \$2}' | sort | uniq -c | sort -n -r | head"
-  fi
+  # Use a better history command.
+  alias history='fc -li'
 }
 
 # endregion --------------------------------------------------------------------
