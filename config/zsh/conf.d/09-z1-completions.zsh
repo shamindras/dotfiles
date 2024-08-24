a0c3793ee066f972a2e587e1d68fa4b6d11b5d7e
Shamindra Shrotriya <shamindra.shrotriya@gmail.com>
2024-08-23 22:02:31 -0500

    refactor: update z1-completions module

--- config/zsh/conf.d/09-z1-completions.zsh
+++ config/zsh/conf.d/09-z1-completions.zsh
@@ -4,6 +4,111 @@
 # region: z1_completions: Setup Zsh completion system
 # ------------------------------------------------------------------------------
 
+# run_compinit: Initialize the built-in Zsh completion system.
+function run_compinit {
+  emulate -L zsh; setopt local_options $__z1_opts
+
+  # Zsh compdump file.
+  typeset -g ZSH_COMPDUMP
+  zstyle -s ':z1:completion' compdump 'ZSH_COMPDUMP' ||
+    ZSH_COMPDUMP=$__zsh_cache_dir/zcompdump
+
+  # Load and initialize the completion system ignoring insecure directories with a
+  # cache time of 20 hours, so it should almost always regenerate the first time a
+  # shell is opened each day.
+  autoload -Uz compinit
+  local comp_files=($ZSH_COMPDUMP(Nmh-20))
+  if (( $#comp_files )); then
+    compinit -i -C -d "$ZSH_COMPDUMP"
+  else
+    compinit -i -d "$ZSH_COMPDUMP"
+    # Ensure $ZSH_COMPDUMP is younger than the cache time even if it isn't regenerated.
+    touch "$ZSH_COMPDUMP"
+  fi
+
+  # Compile zcompdump, if modified, in background to increase startup speed.
+  {
+    if [[ -s "$ZSH_COMPDUMP" && (! -s "${ZSH_COMPDUMP}.zwc" || "$ZSH_COMPDUMP" -nt "${ZSH_COMPDUMP}.zwc") ]]; then
+      if command mkdir "${ZSH_COMPDUMP}.zwc.lock" 2>/dev/null; then
+        zcompile "$ZSH_COMPDUMP"
+        command rmdir  "${ZSH_COMPDUMP}.zwc.lock" 2>/dev/null
+      fi
+    fi
+  } &!
+}
+
+# compstyle_z1_setup: Set Zsh completion styles.
+function compstyle_z1_setup {
+  emulate -L zsh; setopt local_options $__z1_opts
+
+  # Defaults.
+  zstyle ':completion:*:default' list-colors ${(s.:.)LS_COLORS}
+  zstyle ':completion:*:default' list-prompt '%S%M matches%s'
+
+  # Case-insensitive (all), partial-word, and then substring completion.
+  zstyle ':completion:*' matcher-list 'm:{a-zA-Z}={A-Za-z}' 'r:|[._-]=* r:|=*' 'l:|=* r:|=*'
+
+  # Group matches and describe.
+  zstyle ':completion:*:*:*:*:*' menu select
+  zstyle ':completion:*:matches' group 'yes'
+  zstyle ':completion:*:options' description 'yes'
+  zstyle ':completion:*:options' auto-description '%d'
+  zstyle ':completion:*:corrections' format ' %F{red}-- %d (errors: %e) --%f'
+  zstyle ':completion:*:descriptions' format ' %F{purple}-- %d --%f'
+  zstyle ':completion:*:messages' format ' %F{green} -- %d --%f'
+  zstyle ':completion:*:warnings' format ' %F{yellow}-- no matches found --%f'
+  zstyle ':completion:*' format ' %F{blue}-- %d --%f'
+  zstyle ':completion:*' group-name ''
+  zstyle ':completion:*' verbose yes
+
+  # Fuzzy match mistyped completions.
+  zstyle ':completion:*' completer _complete _match _approximate
+  zstyle ':completion:*:match:*' original only
+  zstyle ':completion:*:approximate:*' max-errors 1 numeric
+
+  # Increase the number of errors based on the length of the typed word. But make
+  # sure to cap (at 7) the max-errors to avoid hanging.
+  zstyle -e ':completion:*:approximate:*' max-errors 'reply=($((($#PREFIX+$#SUFFIX)/3>7?7:($#PREFIX+$#SUFFIX)/3))numeric)'
+
+  # Don't complete unavailable commands.
+  zstyle ':completion:*:functions' ignored-patterns '(_*|pre(cmd|exec))'
+
+  # Array completion element sorting.
+  zstyle ':completion:*:*:-subscript-:*' tag-order indexes parameters
+
+  # Directories
+  zstyle ':completion:*:*:cd:*' tag-order local-directories directory-stack path-directories
+  zstyle ':completion:*:*:cd:*:directory-stack' menu yes select
+  zstyle ':completion:*:-tilde-:*' group-order 'named-directories' 'path-directories' 'users' 'expand'
+  zstyle ':completion:*' squeeze-slashes true
+  zstyle ':completion:*' special-dirs ..
+
+  # History
+  zstyle ':completion:*:history-words' stop yes
+  zstyle ':completion:*:history-words' remove-all-dups yes
+  zstyle ':completion:*:history-words' list false
+  zstyle ':completion:*:history-words' menu yes
+
+  # Environment Variables
+  zstyle ':completion::*:(-command-|export):*' fake-parameters ${${${_comps[(I)-value-*]#*,}%%,*}:#-*-}
+
+  # Ignore multiple entries.
+  zstyle ':completion:*:(rm|kill|diff):*' ignore-line other
+  zstyle ':completion:*:rm:*' file-patterns '*:all-files'
+
+  # Kill
+  zstyle ':completion:*:*:*:*:processes' command 'ps -u $LOGNAME -o pid,user,command -w'
+  zstyle ':completion:*:*:kill:*:processes' list-colors '=(#b) #([0-9]#) ([0-9a-z-]#)*=01;36=0=01'
+  zstyle ':completion:*:*:kill:*' menu yes select
+  zstyle ':completion:*:*:kill:*' force-list always
+  zstyle ':completion:*:*:kill:*' insert-ids single
+
+  # complete manual by their section
+  zstyle ':completion:*:manuals'    separate-sections true
+  zstyle ':completion:*:manuals.*'  insert-sections   true
+  zstyle ':completion:*:man:*'      menu yes select
+}
+
 function z1_completions {
   # Set options related to the completions.
   setopt always_to_end     # Move cursor to the end of a completed word.
