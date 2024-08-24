a978cd84e18494c196db03d5eee754e52c00ea67
Shamindra Shrotriya <shamindra.shrotriya@gmail.com>
2024-08-23 21:58:47 -0500

    refactor: include z1-plugin code into separate module

--- config/zsh/conf.d/08-z1-plugins.zsh
+++ config/zsh/conf.d/08-z1-plugins.zsh
@@ -1,84 +1,126 @@
 #!/bin/zsh
 
 # ------------------------------------------------------------------------------
-# region: Utility
+# region: Plugin utilities
 # ------------------------------------------------------------------------------
 
-() {
-  # Use built-in paste magic.
-  autoload -Uz bracketed-paste-url-magic
-  zle -N bracketed-paste bracketed-paste-url-magic
-  autoload -Uz url-quote-magic
-  zle -N self-insert url-quote-magic
+# plugin-clone: Use git to clone Zsh plugins in parallel.
+function plugin-clone {
+  emulate -L zsh; setopt local_options $__z1_opts
 
-  # Load more specific 'run-help' function from $fpath.
-  (( $+aliases[run-help] )) && unalias run-help && autoload -Uz run-help
-  alias help=run-help
+  local repo_dir git_url
+  zstyle -s ':z1:plugins:repos' dir     'repo_dir' || repo_dir=$__zsh_cache_dir/repos
+  zstyle -s ':z1:plugins:repos' git_url 'git_url'  || git_url="https://github.com"
 
-  # Make ls more useful.
-  if (( ! $+commands[dircolors] )) && [[ "$OSTYPE" != darwin* ]]; then
-    # Group dirs first on non-BSD systems
-    alias ls="${aliases[ls]:-ls} --group-directories-first"
-  fi
-  # Show human readable file sizes.
-  alias ls="${aliases[ls]:-ls} -h"
-
-  # Ensure python commands exist.
-  if (( $+commands[python3] )) && ! (( $+commands[python] )); then
-    alias python=python3
-  fi
-  if (( $+commands[pip3] )) && ! (( $+commands[pip] )); then
-    alias pip=pip3
-  fi
-
-  # Ensure envsubst command exists.
-  if ! (( $+commands[envsubst] )); then
-    alias envsubst="python -c 'import os,sys;[sys.stdout.write(os.path.expandvars(l)) for l in sys.stdin]'"
-  fi
-
-  # Ensure hd (hex dump) command exists.
-  if ! (( $+commands[hd] )) && (( $+commands[hexdump] )); then
-    alias hd="hexdump -C"
-  fi
-
-  # Ensure open command exists.
-  if ! (( $+commands[open] )); then
-    if [[ "$OSTYPE" == cygwin* ]]; then
-      alias open='cygstart'
-    elif [[ "$OSTYPE" == linux-android ]]; then
-      alias open='termux-open'
-    elif (( $+commands[xdg-open] )); then
-      alias open='xdg-open'
+  local repo repo_url plugin_dir
+  local -Ua repos=($@)
+  for repo in $repos; do
+    if [[ $repo == (http(|s)://|git@)* ]]; then
+      repo_url=$repo
+    else
+      repo_url=$git_url/$repo
     fi
-  fi
 
-  # Ensure pbcopy/pbpaste commands exist.
-  if ! (( $+commands[pbcopy] )); then
-    if [[ "$OSTYPE" == cygwin* ]]; then
-      alias pbcopy='tee > /dev/clipboard'
-      alias pbpaste='cat /dev/clipboard'
-    elif [[ "$OSTYPE" == linux-android ]]; then
-      alias pbcopy='termux-clipboard-set'
-      alias pbpaste='termux-clipboard-get'
-    elif (( $+commands[wl-copy] && $+commands[wl-paste] )); then
-      alias pbcopy='wl-copy'
-      alias pbpaste='wl-paste'
-    elif [[ -n $DISPLAY ]]; then
-      if (( $+commands[xclip] )); then
-        alias pbcopy='xclip -selection clipboard -in'
-        alias pbpaste='xclip -selection clipboard -out'
-      elif (( $+commands[xsel] )); then
-        alias pbcopy='xsel --clipboard --input'
-        alias pbpaste='xsel --clipboard --output'
-      fi
+    plugin_dir=$repo_dir/${${repo_url:t}%.git}
+    if [[ ! -d $plugin_dir ]]; then
+      echo "Cloning $repo..."
+      (
+        command git clone --quiet --depth 1 --recursive --shallow-submodules $repo_url $plugin_dir
+        plugin-compile $plugin_dir
+      ) &
     fi
-  fi
+  done
+  wait
+}
 
-  # Cross platform `sed -i` syntax.
-  function sedi {
-    # GNU/BSD
-    sed --version &>/dev/null && sed -i -- "$@" || sed -i "" "$@"
-  }
+# plugin-home: Show the plugin home directory
+function plugin-home {
+  emulate -L zsh; setopt local_options $__z1_opts
+  local repo_dir
+  zstyle -s ':z1:plugins:repos' dir 'repo_dir' || repo_dir=$__zsh_cache_dir/repos
+  print -r -- $repo_dir
+}
+
+# plugin-list: List cloned plugins.
+function plugin-list {
+  emulate -L zsh; setopt local_options $__z1_opts
+  local repo_dir plugin_dir
+  zstyle -s ':z1:plugins:repos' dir 'repo_dir' || repo_dir=$__zsh_cache_dir/repos
+  for plugin_dir in $repo_dir/*/.git(N/); do
+    print -r -- ${plugin_dir:A:h:t}
+  done
+}
+
+# plugin-update: Use git to pull updates to Zsh plugins.
+function plugin-update {
+  emulate -L zsh; setopt local_options $__z1_opts
+  local repo_dir
+  zstyle -s ':z1:plugins:repos' dir 'repo_dir' || repo_dir=$__zsh_cache_dir/repos
+
+  local plugin_dir oldsha newsha
+  for plugin_dir in $repo_dir/*/.git(N/); do
+    plugin_dir=${plugin_dir:A:h}
+    echo "Updating ${plugin_dir:t}..."
+    (
+      oldsha=$(command git -C $plugin_dir rev-parse --short HEAD)
+      command git -C $plugin_dir pull --quiet --ff --depth 1 --rebase --autostash
+      newsha=$(command git -C $plugin_dir rev-parse --short HEAD)
+      [[ $oldsha == $newsha ]] || echo "Plugin updated: $plugin_dir:t ($oldsha -> $newsha)"
+    ) &
+  done
+  wait
+  plugin-compile
+  echo "Update complete."
+}
+
+# plugin-compile: Compile plugins.
+function plugin-compile {
+  emulate -L zsh; setopt local_options $__z1_opts
+  local repo_dir
+  zstyle -s ':z1:plugins:repos' dir 'repo_dir' || repo_dir=$__zsh_cache_dir/repos
+  autoload -Uz zrecompile
+  local zfile
+  for zfile in ${1:-$repo_dir}/**/*.zsh{,-theme}(N); do
+    [[ $zfile != */test-data/* ]] || continue
+    zrecompile -pq "$zfile"
+  done
+}
+
+# z1_plugins: Setup Zsh plugins.
+function z1_plugins {
+  emulate -L zsh; setopt local_options $__z1_opts
+
+  local repo_dir giturl
+  zstyle -s ':z1:repos' repo_dir 'repo_dir' || repo_dir=$__zsh_cache_dir/repos
+  zstyle -s ':z1:plugins' git_url  'git_url'  || git_url="https://github.com"
+
+  # Get all the different plugin types.
+  local -a {clone,path,fpath,zsh,defer}_plugins
+  zstyle -a ':z1:plugins:load:kind' clone 'clone_plugins'
+  zstyle -a ':z1:plugins:load:kind' path  'path_plugins'
+  zstyle -a ':z1:plugins:load:kind' fpath 'fpath_plugins'
+  zstyle -a ':z1:plugins:load:kind' zsh   'zsh_plugins'
+  zstyle -a ':z1:plugins:load:kind' defer 'defer_plugins'
+
+  # Combine for cloning missing ones.
+  local -a plugins=($clone_plugins $path_plugins $fpath_plugins $zsh_plugins $defer_plugins)
+
+  # Remove bare words ${(M)plugins:#*/*} and paths with leading slash ${plugins:#/*}.
+  # Then split/join to keep the 2-part user/repo form to bulk-clone repos.
+  local repo; local -aU repos
+  for repo in ${${(M)plugins:#*/*}:#/*}; do
+    repo=${(@j:/:)${(@s:/:)repo}[1,2]}
+    if [[ ! -d $ZSH_REPO_HOME/$repo ]]; then
+      (
+        command git clone -q --depth 1 --recursive --shallow-submodules \
+          $git_url/$repo $ZSH_REPO_HOME/$repo
+          plugin-compile $ZSH_REPO_HOME/$repo
+      ) &
+    fi
+  done
+  wait
 }
 
 # endregion --------------------------------------------------------------------
+
+# vim: ft=zsh 
