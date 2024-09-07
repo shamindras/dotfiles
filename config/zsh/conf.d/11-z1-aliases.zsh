8b686115c96b8f9becd70b104b289c46333512c9
Shamindra Shrotriya <shamindra.shrotriya@gmail.com>
2024-09-08 07:57:36 +1000

    refactor: remove git gone alias. It is in the main git config

--- config/zsh/conf.d/11-z1-aliases.zsh
+++ config/zsh/conf.d/11-z1-aliases.zsh
@@ -48,7 +48,6 @@ function z1_aliases {
     # source: https://github.com/dlvhdr/dotfiles/blob/main/.config/zsh/aliases.zsh
     alias g="git"
     alias gaa="git add --all"
-    alias ggn="git fetch -p && git branch -vv | grep 'origin/.*: gone]' | awk '{print $1}' | xargs git branch -D"
     alias gcb="git checkout -b"
     alias glg="git log --graph --abbrev-commit --decorate --format=format:'%C(yellow)%h%C(reset) %C(white)%s%C(reset) %C(dim white)-%C(reset) %ar %C(dim white)<%an>%C(reset)%C(auto)%d%C(reset)%n' --all --stat"
     alias glg2="git log --graph --abbrev-commit --decorate --format=format:'%C(yellow)%h%C(reset) %C(white)%s%C(reset) %C(dim white)-%C(reset) %ar %C(dim white)<%an>%C(reset)%C(auto)%d%C(reset)' --all"
