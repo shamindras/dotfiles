4817fe378e0beb7497f494c03ca543668817ebf9
Shamindra Shrotriya <shamindra.shrotriya@gmail.com>
2024-09-09 16:46:41 +1000

    fix: quotes in aliases

--- config/zsh/conf.d/11-z1-aliases.zsh
+++ config/zsh/conf.d/11-z1-aliases.zsh
@@ -10,11 +10,11 @@ function z1_aliases {
     alias ..='z ..'
     alias ...='z ../..'
     alias ....='z ../../..'
-    alias .2='z ../../'                     # Go back 3 directory levels
-    alias .3='z ../../../'                     # Go back 3 directory levels
-    alias .4='z ../../../../'                  # Go back 4 directory levels
-    alias .5='z ../../../../../'               # Go back 5 directory levels
-    alias .6='z ../../../../../../'            # Go back 6 directory levels
+    alias .2='z ../../'                        # Go up 3 dirs
+    alias .3='z ../../../'                     # Go up 3 dirs
+    alias .4='z ../../../../'                  # Go up 4 dirs
+    alias .5='z ../../../../../'               # Go up 5 dirs
+    alias .6='z ../../../../../../'            # Go up 6 dirs
     alias b='bat --style=grid,numbers'
     alias bat='bat --style=grid,numbers'
     alias brewup='brew update && brew upgrade && brew cleanup'
@@ -37,9 +37,9 @@ function z1_aliases {
     alias rcp='rsync -ah --info=progress2'
     alias rmi='rm -i'
     alias rmf='rm -rf'
-    alias sc='source ${XDG_CONFIG_HOME}/zsh/.zshrc' # source ~/.zshrc
+    alias sc="source ${XDG_CONFIG_HOME}/zsh/.zshrc" # source ~/.zshrc
     alias top='btop;clear;'
-    alias tree='eza --tree --all --group-directories-first -I '.git|.svn|.hg|.idea|.vscode|.Rproj.user|.pytest_cache''
+    alias tree='eza --tree --all --group-directories-first -I ".git|.svn|.hg|.idea|.vscode|.Rproj.user|.pytest_cache"'
     alias t1='tree --level=1'
     alias tl1='t1 --long'
     alias t2='tree --level=2'
@@ -52,8 +52,8 @@ function z1_aliases {
     alias g='git'
     alias gaa='git add --all'
     alias gcb='git checkout -b'
-    alias glg='git log --graph --abbrev-commit --decorate --format=format:'%C(yellow)%h%C(reset) %C(white)%s%C(reset) %C(dim white)-%C(reset) %ar %C(dim white)<%an>%C(reset)%C(auto)%d%C(reset)%n' --all --stat'
-    alias glg2='git log --graph --abbrev-commit --decorate --format=format:'%C(yellow)%h%C(reset) %C(white)%s%C(reset) %C(dim white)-%C(reset) %ar %C(dim white)<%an>%C(reset)%C(auto)%d%C(reset)' --all'
+    alias glg="git log --graph --abbrev-commit --decorate --format=format:'%C(yellow)%h%C(reset) %C(white)%s%C(reset) %C(dim white)-%C(reset) %ar %C(dim white)<%an>%C(reset)%C(auto)%d%C(reset)%n' --all --stat"
+    alias glg2="git log --graph --abbrev-commit --decorate --format=format:'%C(yellow)%h%C(reset) %C(white)%s%C(reset) %C(dim white)-%C(reset) %ar %C(dim white)<%an>%C(reset)%C(auto)%d%C(reset)' --all"
     alias gsc='git switch -c'
     alias gsc='git switch main'
     alias gsmp='git switch main && git pull origin main'
@@ -67,7 +67,7 @@ function z1_aliases {
 
     # source: running `xdg-ninja` in `$HOME` gave these suggestion
     # subversion 
-    alias svn='svn --config-dir $XDG_CONFIG_HOME/subversion'
+    alias svn="svn --config-dir ${XDG_CONFIG_HOME}/subversion"
     alias gpg='${aliases[gpg]:-gpg} --homedir \"\$GNUPGHOME\"'
     alias wget='${aliases[wget]:-wget} --hsts-file=\"\$XDG_CACHE_HOME/wget/wget-hsts\"'
 
@@ -75,11 +75,11 @@ function z1_aliases {
     # Source: https://github.com/ameensol/shell/blob/master/.zshenv#L29-L30
     # clean up current directory
     # TODO use `fd` instead of `find` to speed this up
-    alias clean='find . -maxdepth 1 -name '*.DS_Store' -o -name '*~' -o -name '.*~' -o -name '*.swo' -o -name '*.swp' -o -name '.*.swo' | xargs rm'
+    alias clean="find . -maxdepth 1 -name '*.DS_Store' -o -name '*~' -o -name '.*~' -o -name '*.swo' -o -name '*.swp' -o -name '.*.swo' | xargs rm"
 
     # clean up current directory AND all subdirectories
     # TODO use `fd` instead of `find` to speed this up
-    alias wash='find . -name '*.DS_Store' -o -name '*~' -o -name '.*~' -o -name '*.swo' -o -name '*.swp' -o -name '.*.swo' | xargs rm'
+    alias wash="find . -name '*.DS_Store' -o -name '*~' -o -name '.*~' -o -name '*.swo' -o -name '*.swp' -o -name '.*.swo' | xargs rm"
 }
 
 # Application open suffix alias --
@@ -93,8 +93,8 @@ function z1_suffix_aliases {
     alias -s {html,HTML}='open -gja Google\ Chrome.app' # TODO use `firefox` instead of `Chrome`
     alias -s {mp4,MP4,mov,MOV,mkv,MKV}='open -gja VLC.app'
     alias -s {zip,ZIP,war,WAR}='unzip -l'
-    alias -s gz='tar -tf'
-    alias -s {tgz,TGZ}='tar -tf'
+    alias -s gz="tar -tf"
+    alias -s {tgz,TGZ}="tar -tf"
 }
 
 # endregion --------------------------------------------------------------------
