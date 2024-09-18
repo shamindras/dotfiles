8978c1aa736c271e037b1604cd8540668fa83987
Shamindra Shrotriya <shamindra.shrotriya@gmail.com>
2024-09-18 20:48:51 +1000

    refactor(zsh): remove unused functions

--- config/zsh/conf.d/05-z1-editor.zsh
+++ config/zsh/conf.d/05-z1-editor.zsh
@@ -4,44 +4,6 @@
 # region: Setup editor and keybinds
 # ------------------------------------------------------------------------------
 
-function z1_editor_no_ps2 {
-  emulate -L zsh
-  local -r func=no-ps2-test
-
-  if (( ${+functions[$func]} )); then
-    unfunction -- $func
-  fi
-
-  if functions[$func]=$PREBUFFER$BUFFER 2>/dev/null &&
-    (( ${+functions[$func]} )); then
-    unfunction -- $func
-    if functions[$func]=$PREBUFFER$BUFFER$'\ndo\ndone' 2>/dev/null &&
-      (( ${+functions[$func]} )) ; then
-      unfunction -- $func
-    else
-      local w
-      zstyle -s 'z1:editor:no-ps2:' accept-line w || w=accept-line
-      if [[ -n $w ]]; then
-        builtin zle -- $w
-      fi
-      return
-    fi
-  fi
-
-  LBUFFER+=$'\n'
-}
-
-function z1_editor {
-  # Allow mapping Ctrl+S and Ctrl+Q shortcuts
-  [[ -r ${TTY:-} && -w ${TTY:-} && $+commands[stty] == 1 ]] && stty -ixon <$TTY >$TTY
-
-  # No PS2
-  builtin zle -N z1_editor_no_ps2
-  bindkey '^J' z1_editor_no_ps2
-  bindkey '^M' z1_editor_no_ps2
-  bindkey -e
-}
-
 # Set up vi style keybindings
 # source: https://github.com/mrnugget/dotfiles/blob/c4624ed521d539856bcf764f04a295bb19093566/zshrc#L67C1-L116C25
 function z1_vi_style_keybindings {
