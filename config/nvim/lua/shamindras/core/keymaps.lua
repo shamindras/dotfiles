fcd308ac7aa9cffc6f3c5f90d8adeb3e5a8cbd9b
Shamindra Shrotriya <shamindra.shrotriya@gmail.com>
2025-01-04 16:41:52 -0600

    refactor(nvim): clean up unused keymaps, add window maximizer, apply style

--- config/nvim/lua/shamindras/core/keymaps.lua
+++ config/nvim/lua/shamindras/core/keymaps.lua
@@ -67,45 +67,15 @@ keymap('n', 'C', '"_C')
 keymap('v', '>', '>gv^')
 keymap('v', '<', '<gv^')
 
--- move Lines
--- TODO: get the `A` key mapped to left option key in macOS on wezterm
---       currently we use `CMD+Option` on macOS keyboard.
--- keymap("n", "<A-j>", "<cmd>execute 'move .+' . v:count1<cr>==", { desc = "Move Down" })
--- keymap("n", "<A-k>", "<cmd>execute 'move .-' . (v:count1 + 1)<cr>==", { desc = "Move Up" })
--- keymap("i", "<A-j>", "<esc><cmd>m .+1<cr>==gi", { desc = "Move Down" })
--- keymap("i", "<A-k>", "<esc><cmd>m .-2<cr>==gi", { desc = "Move Up" })
--- keymap("v", "<A-j>", ":<C-u>execute \"'<,'>move '>+\" . v:count1<cr>gv=gv", { desc = "Move Down" })
--- keymap("v", "<A-k>", ":<C-u>execute \"'<,'>move '<-\" . (v:count1 + 1)<cr>gv=gv", { desc = "Move Up" })
-
 -- ------------------------------------------------------------------------- }}}
 
 -- {{{ navigation
 
 -- better up/down
-keymap(
-  { 'n', 'x' },
-  'j',
-  "v:count == 0 ? 'gj' : 'j'",
-  { desc = 'Down', expr = true }
-)
-keymap(
-  { 'n', 'x' },
-  '<Down>',
-  "v:count == 0 ? 'gj' : 'j'",
-  { desc = 'Down', expr = true }
-)
-keymap(
-  { 'n', 'x' },
-  'k',
-  "v:count == 0 ? 'gk' : 'k'",
-  { desc = 'Up', expr = true }
-)
-keymap(
-  { 'n', 'x' },
-  '<Up>',
-  "v:count == 0 ? 'gk' : 'k'",
-  { desc = 'Up', expr = true }
-)
+keymap({ 'n', 'x' }, 'j', "v:count == 0 ? 'gj' : 'j'", { desc = 'Down', expr = true })
+keymap({ 'n', 'x' }, '<Down>', "v:count == 0 ? 'gj' : 'j'", { desc = 'Down', expr = true })
+keymap({ 'n', 'x' }, 'k', "v:count == 0 ? 'gk' : 'k'", { desc = 'Up', expr = true })
+keymap({ 'n', 'x' }, '<Up>', "v:count == 0 ? 'gk' : 'k'", { desc = 'Up', expr = true })
 
 -- Map H and L to ^ and $, respectively
 -- source: https://github.com/famiu/dot-nvim/blob/d7922d6ce9d9483cd68c67abb883e8ab91a17e4f/lua/keybinds.lua#L4-L6
@@ -114,78 +84,48 @@ keymap('n', 'H', '^')
 keymap('n', 'L', '$')
 
 -- https://github.com/mhinz/vim-galore#saner-behavior-of-n-and-n
-keymap(
-  'n',
-  'n',
-  "'Nn'[v:searchforward].'zv'",
-  { expr = true, desc = 'Next Search Result' }
-)
-keymap(
-  'x',
-  'n',
-  "'Nn'[v:searchforward]",
-  { expr = true, desc = 'Next Search Result' }
-)
-keymap(
-  'o',
-  'n',
-  "'Nn'[v:searchforward]",
-  { expr = true, desc = 'Next Search Result' }
-)
-keymap(
-  'n',
-  'N',
-  "'nN'[v:searchforward].'zv'",
-  { expr = true, desc = 'Prev Search Result' }
-)
-keymap(
-  'x',
-  'N',
-  "'nN'[v:searchforward]",
-  { expr = true, desc = 'Prev Search Result' }
-)
-keymap(
-  'o',
-  'N',
-  "'nN'[v:searchforward]",
-  { expr = true, desc = 'Prev Search Result' }
-)
-
--- Move to window using the <ctrl> hjkl keys
-keymap(
-  'n',
-  '<C-h>',
-  '<C-w>h',
-  { desc = 'Go to Left Window', remap = true, noremap = false }
-)
-keymap(
-  'n',
-  '<C-j>',
-  '<C-w>j',
-  { desc = 'Go to Lower Window', remap = true, noremap = false }
-)
-keymap(
-  'n',
-  '<C-k>',
-  '<C-w>k',
-  { desc = 'Go to Upper Window', remap = true, noremap = false }
-)
-keymap(
-  'n',
-  '<C-l>',
-  '<C-w>l',
-  { desc = 'Go to Right Window', remap = true, noremap = false }
-)
+keymap('n', 'n', "'Nn'[v:searchforward].'zv'", { expr = true, desc = 'Next Search Result' })
+keymap('x', 'n', "'Nn'[v:searchforward]", { expr = true, desc = 'Next Search Result' })
+keymap('o', 'n', "'Nn'[v:searchforward]", { expr = true, desc = 'Next Search Result' })
+keymap('n', 'N', "'nN'[v:searchforward].'zv'", { expr = true, desc = 'Prev Search Result' })
+keymap('x', 'N', "'nN'[v:searchforward]", { expr = true, desc = 'Prev Search Result' })
+keymap('o', 'N', "'nN'[v:searchforward]", { expr = true, desc = 'Prev Search Result' })
 
 -- ------------------------------------------------------------------------- }}}
 
 -- {{{ windows
 
+---Toggle the maximization state of the current window.
+---Maximizes the window if it's not maximized, restores the previous layout if it is.
+---Preserves the cursor and scroll position of the window.
+---@return nil
+local function toggle_window_maximize()
+  local current_win = vim.api.nvim_get_current_win()
+  local current_view = vim.fn.winsaveview()
+
+  -- Store the current window configuration
+  local is_maximized = vim.t.maximized_window == current_win
+
+  if is_maximized then
+    -- Restore the previous window layout
+    vim.cmd 'wincmd ='
+    vim.t.maximized_window = nil
+  else
+    -- Maximize the current window
+    vim.cmd 'wincmd |' -- Maximize vertically
+    vim.cmd 'wincmd _' -- Maximize horizontally
+    vim.t.maximized_window = current_win
+  end
+
+  -- Restore the view (cursor position, scroll position, etc.)
+  vim.fn.winrestview(current_view)
+end
+
 -- window management
--- TODO: add window maximizer toggle
-keymap('n', '<leader>wv', '<C-w>v') -- split window vertically
-keymap('n', '<leader>wh', '<C-w>s') -- split window horizontally
 keymap('n', '<leader>we', '<C-w>=') -- make split windows equal width & height
+keymap('n', '<leader>wh', '<C-w>s') -- split window horizontally
+keymap('n', '<leader>wm', toggle_window_maximize) -- toggle maximize active window
+keymap('n', '<leader>wv', '<C-w>v') -- split window vertically
 keymap('n', '<leader>wx', '<cmd>close<CR>') -- close current split window
 
 -- ------------------------------------------------------------------------- }}}
