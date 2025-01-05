faea23e50cca2ee61652b63b64d887905e19f688
Shamindra Shrotriya <shamindra.shrotriya@gmail.com>
2025-01-04 21:41:04 -0600

    chore(nvim): style lua

--- config/nvim/lua/shamindras/plugins/treesitter.lua
+++ config/nvim/lua/shamindras/plugins/treesitter.lua
@@ -3,7 +3,7 @@ return {
     'nvim-treesitter/nvim-treesitter',
     build = ':TSUpdate',
     main = 'nvim-treesitter.configs', -- Sets main module to use for opts
-    event = "BufReadPost",
+    event = 'BufReadPost',
     -- [[ Configure Treesitter ]] See `:help nvim-treesitter`
     opts = {
       -- Add languages to be installed here that you want installed for treesitter
@@ -65,4 +65,3 @@ return {
 }
 
 -- vim: ts=2 sts=2 sw=2 et
-
