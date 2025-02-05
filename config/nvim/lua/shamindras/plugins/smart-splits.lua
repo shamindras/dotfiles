06c7ec41ef63fa0ac60b1b2fc247bf47c8273e16
Shamindra Shrotriya <shamindra.shrotriya@gmail.com>
2025-02-05 13:23:24 -0600

    refactor(nvim): use lazy loading

--- config/nvim/lua/shamindras/plugins/smart-splits.lua
+++ config/nvim/lua/shamindras/plugins/smart-splits.lua
@@ -1,20 +1,85 @@
-return { -- Fuzzy Finder (files, lsp, etc)
+return {
   'mrjones2014/smart-splits.nvim',
-  lazy = false,
+  lazy = true, -- Change to true since we'll load it on demand
+  event = { 'WinNew', 'WinEnter' },
+  keys = {
+    -- Moving between splits
+    {
+      '<C-h>',
+      function()
+        require('smart-splits').move_cursor_left()
+      end,
+      mode = 'n',
+      desc = 'Move to left split',
+    },
+    {
+      '<C-j>',
+      function()
+        require('smart-splits').move_cursor_down()
+      end,
+      mode = 'n',
+      desc = 'Move to bottom split',
+    },
+    {
+      '<C-k>',
+      function()
+        require('smart-splits').move_cursor_up()
+      end,
+      mode = 'n',
+      desc = 'Move to top split',
+    },
+    {
+      '<C-l>',
+      function()
+        require('smart-splits').move_cursor_right()
+      end,
+      mode = 'n',
+      desc = 'Move to right split',
+    },
+    {
+      '<C-\\>',
+      function()
+        require('smart-splits').move_cursor_previous()
+      end,
+      mode = 'n',
+      desc = 'Move to previous split',
+    },
+
+    -- Swapping buffers
+    {
+      '<leader>wH',
+      function()
+        require('smart-splits').swap_buf_left()
+      end,
+      mode = 'n',
+      desc = 'Swap with left buffer',
+    },
+    {
+      '<leader>wJ',
+      function()
+        require('smart-splits').swap_buf_down()
+      end,
+      mode = 'n',
+      desc = 'Swap with bottom buffer',
+    },
+    {
+      '<leader>wK',
+      function()
+        require('smart-splits').swap_buf_up()
+      end,
+      mode = 'n',
+      desc = 'Swap with top buffer',
+    },
+    {
+      '<leader>wL',
+      function()
+        require('smart-splits').swap_buf_right()
+      end,
+      mode = 'n',
+      desc = 'Swap with right buffer',
+    },
+  },
   config = function()
     require('smart-splits').setup()
-
-    -- moving between splits
-    vim.keymap.set('n', '<C-h>', require('smart-splits').move_cursor_left)
-    vim.keymap.set('n', '<C-j>', require('smart-splits').move_cursor_down)
-    vim.keymap.set('n', '<C-k>', require('smart-splits').move_cursor_up)
-    vim.keymap.set('n', '<C-l>', require('smart-splits').move_cursor_right)
-    vim.keymap.set('n', '<C-\\>', require('smart-splits').move_cursor_previous)
-
-    -- swapping buffers between windows
-    vim.keymap.set('n', '<leader>wH', require('smart-splits').swap_buf_left)
-    vim.keymap.set('n', '<leader>wJ', require('smart-splits').swap_buf_down)
-    vim.keymap.set('n', '<leader>wK', require('smart-splits').swap_buf_up)
-    vim.keymap.set('n', '<leader>wL', require('smart-splits').swap_buf_right)
   end,
 }
