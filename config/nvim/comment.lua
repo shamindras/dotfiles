66a615f6663e4d1aec193071e4de2da8f7c8df54
Shamindra Shrotriya <shamindra.shrotriya@gmail.com>
2025-02-05 19:16:44 -0600

    refactor(nvim): revert comment nvim lazy event

--- config/nvim/comment.lua
+++ config/nvim/comment.lua
@@ -1,6 +1,5 @@
 -- Easily comment visual regions/lines
 return {
   'numToStr/Comment.nvim',
-  event = { 'BufRead', 'InsertEnter' }, -- Load on BufRead (opening a file) or InsertEnter
   opts = {},
 }
