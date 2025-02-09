948337ee7503b1850ab374efa3ba2fb046428395
Shamindra Shrotriya <shamindra.shrotriya@gmail.com>
2025-02-09 09:56:02 -0600

    style(lua): update wezterm files

--- config/wezterm/utils/keybindings.lua
+++ config/wezterm/utils/keybindings.lua
@@ -29,15 +29,9 @@ local function split_nav(resize_or_move, key)
         }, pane)
       else
         if resize_or_move == 'resize' then
-          win:perform_action(
-            { AdjustPaneSize = { direction_keys[key], 3 } },
-            pane
-          )
+          win:perform_action({ AdjustPaneSize = { direction_keys[key], 3 } }, pane)
         else
-          win:perform_action(
-            { ActivatePaneDirection = direction_keys[key] },
-            pane
-          )
+          win:perform_action({ ActivatePaneDirection = direction_keys[key] }, pane)
         end
       end
     end),
--- config/wezterm/utils/tab_title.lua
+++ config/wezterm/utils/tab_title.lua
@@ -18,42 +18,38 @@ local function get_tab_title(tab_info)
 end
 
 function M.setup()
-  wezterm.on(
-    'format-tab-title',
-    function(tab, tabs, panes, config, hover, max_width)
-      local edge_background = '#0b0022'
-      local background = '#1b1032'
-      local foreground = '#808080'
+  wezterm.on('format-tab-title', function(tab, tabs, panes, config, hover, max_width)
+    local edge_background = '#0b0022'
+    local background = '#1b1032'
+    local foreground = '#808080'
 
-      if tab.is_active then
-        background = '#2b2042'
-        foreground = '#c0c0c0'
-      elseif hover then
-        background = '#3b3052'
-        foreground = '#909090'
-      end
-
-      local edge_foreground = background
-      local title = get_tab_title(tab)
-
-      -- ensure that the titles fit in the available space,
-      -- and that we have room for the edges.
-      title = wezterm.truncate_left(title, max_width - 2)
-
-      return {
-        { Background = { Color = edge_background } },
-        { Foreground = { Color = edge_foreground } },
-        { Text = SOLID_LEFT_ARROW },
-        { Background = { Color = background } },
-        { Foreground = { Color = foreground } },
-        { Text = title },
-        { Background = { Color = edge_background } },
-        { Foreground = { Color = edge_foreground } },
-        { Text = SOLID_RIGHT_ARROW },
-      }
+    if tab.is_active then
+      background = '#2b2042'
+      foreground = '#c0c0c0'
+    elseif hover then
+      background = '#3b3052'
+      foreground = '#909090'
     end
-  )
+
+    local edge_foreground = background
+    local title = get_tab_title(tab)
+
+    -- ensure that the titles fit in the available space,
+    -- and that we have room for the edges.
+    title = wezterm.truncate_left(title, max_width - 2)
+
+    return {
+      { Background = { Color = edge_background } },
+      { Foreground = { Color = edge_foreground } },
+      { Text = SOLID_LEFT_ARROW },
+      { Background = { Color = background } },
+      { Foreground = { Color = foreground } },
+      { Text = title },
+      { Background = { Color = edge_background } },
+      { Foreground = { Color = edge_foreground } },
+      { Text = SOLID_RIGHT_ARROW },
+    }
+  end)
 end
 
 return M
-
