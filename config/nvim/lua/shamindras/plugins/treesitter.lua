3f360380999c9a4186c5a9635085f8528186f96a
Shamindra Shrotriya <shamindra.shrotriya@gmail.com>
2025-02-09 15:27:03 -0600

    chore(nvim): clean treesitter comments

--- config/nvim/lua/shamindras/plugins/treesitter.lua
+++ config/nvim/lua/shamindras/plugins/treesitter.lua
@@ -4,7 +4,6 @@ return {
     build = ':TSUpdate',
     main = 'nvim-treesitter.configs', -- Sets main module to use for opts
     event = 'VeryLazy',
-    -- [[ Configure Treesitter ]] See `:help nvim-treesitter`
     opts = {
       -- Add languages to be installed here that you want installed for treesitter
       ensure_installed = {
@@ -55,12 +54,6 @@ return {
         },
       },
     },
-    -- There are additional nvim-treesitter modules that you can use to interact
-    -- with nvim-treesitter. You should go explore a few and see what interests you:
-    --
-    --    - Incremental selection: Included, see `:help nvim-treesitter-incremental-selection-mod`
-    --    - Show your current context: https://github.com/nvim-treesitter/nvim-treesitter-context
-    --    - Treesitter + textobjects: https://github.com/nvim-treesitter/nvim-treesitter-textobjects
   },
 }
 
