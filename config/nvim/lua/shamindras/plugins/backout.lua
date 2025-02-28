cdf1752c5073326d14c758b006ff9e1b43ddf38c
Shamindra Shrotriya <shamindra.shrotriya@gmail.com>
2025-02-28 10:10:29 -0600

    chore(nvim): clean comments

--- config/nvim/lua/shamindras/plugins/backout.lua
+++ config/nvim/lua/shamindras/plugins/backout.lua
@@ -1,9 +1,8 @@
 return {
   'AgusDOLARD/backout.nvim',
-  event = { 'InsertEnter', 'CmdlineEnter' }, -- Load when entering insert mode or cmdline
+  event = { 'InsertEnter', 'CmdlineEnter' },
   opts = {},
   keys = {
-    -- Define your keybinds
     { '<A-h>', "<cmd>lua require('backout').back()<cr>", mode = { 'i', 'c' } },
     { '<A-l>', "<cmd>lua require('backout').out()<cr>", mode = { 'i', 'c' } },
   },
