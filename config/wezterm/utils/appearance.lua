18753fc0b67ff53c466284d19f7d597a3ff20238
Shamindra Shrotriya <shamindra.shrotriya@gmail.com>
2025-08-03 22:07:36 +1000

    refactor(wezterm): update font

--- config/wezterm/utils/appearance.lua
+++ config/wezterm/utils/appearance.lua
@@ -22,7 +22,9 @@ function M.setup(config)
     -- wezterm.font { family = 'MonaspiceAr Nerd Font', weight = 'Regular' }
     -- wezterm.font { family = 'Iosevka Nerd Font Mono', weight = 'Regular' }
     -- wezterm.font({ family = 'Maple Mono', weight = 'Regular' })
-    wezterm.font({ family = 'JetBrainsMono Nerd Font', weight = 'Regular' })
+    -- wezterm.font({ family = 'Cascadia Code NF', weight = 'Regular' })
+    -- wezterm.font({ family = 'JetBrainsMono Nerd Font', weight = 'Regular' })
+    wezterm.font({ family = 'CommitMono Nerd Font', weight = 'Regular' })
   config.font_size = 18
 
   -- Color scheme and window appearance
