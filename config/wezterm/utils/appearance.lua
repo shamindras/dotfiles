0672b25fa0011ee5940bdaf999774050a8f9eaf9
Shamindra Shrotriya <shamindra.shrotriya@gmail.com>
2025-09-25 18:15:33 +0530

    refactor(wezterm): remove tab bar

--- config/wezterm/utils/appearance.lua
+++ config/wezterm/utils/appearance.lua
@@ -36,7 +36,7 @@ function M.setup(config)
   })
 
   -- Window configuration
-  config.enable_tab_bar = true
+  config.enable_tab_bar = false
   config.window_decorations = 'RESIZE'
   config.window_background_opacity = 0.90
   config.macos_window_background_blur = 4
--- config/wezterm/wezterm.lua
+++ config/wezterm/wezterm.lua
@@ -5,13 +5,13 @@ local wezterm = require('wezterm')
 local appearance = require('utils.appearance')
 local keybindings = require('utils.keybindings')
 local core = require('utils.core')
-local tab_title = require('utils.tab_title')
+-- local tab_title = require('utils.tab_title')
 
 -- Initialize configuration
 local config = wezterm.config_builder()
 
 -- Set up tab title formatting
-tab_title.setup()
+-- tab_title.setup()
 
 -- Apply module configurations
 config = appearance.setup(config)
