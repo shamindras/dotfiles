12fe979e5c818c7bfb4296a6b7815edcdab380af
Shamindra Shrotriya <shamindra.shrotriya@gmail.com>
2025-12-19 13:06:03 +1000

    refactor(nvim): update treesitter module loading

--- config/nvim/lua/shamindras/plugins/treesitter.lua
+++ config/nvim/lua/shamindras/plugins/treesitter.lua
@@ -3,47 +3,30 @@ return {
     'nvim-treesitter/nvim-treesitter',
     branch = 'main',
     build = ':TSUpdate',
-    lazy = false, -- Ensure it loads immediately
   },
   {
     'MeanderingProgrammer/treesitter-modules.nvim',
     dependencies = { 'nvim-treesitter/nvim-treesitter' },
-    lazy = false, -- Don't lazy load
+    event = { 'BufReadPost', 'BufNewFile' },
     opts = {
       -- Add languages to be installed here that you want installed for treesitter
       ensure_installed = {
-        'bash',
-        'cmake',
-        -- 'css',
-        'dockerfile',
-        'gitignore',
-        'go',
         'html',
-        'java',
-        'javascript',
         'json',
         'lua',
         'make',
         'markdown',
         'markdown_inline',
         'python',
-        'regex',
-        'sql',
-        -- 'terraform',
         'toml',
-        -- 'tsx',
-        -- 'typescript',
-        'vim',
-        'vimdoc',
         'yaml',
       },
       -- Autoinstall languages that are not installed
       auto_install = true,
       highlight = {
         enable = true,
-        additional_vim_regex_highlighting = { 'ruby' },
       },
-      indent = { enable = true, disable = { 'ruby' } },
+      indent = { enable = true },
       incremental_selection = {
         enable = true,
         keymaps = {
