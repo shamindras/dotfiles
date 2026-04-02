58e5d8af4cdbc94c2039107c4286f3b09027d0e9
Shamindra Shrotriya <shamindra.shrotriya@gmail.com>
2026-04-03 03:38:03 +1000

    fix(nvim): add lazydev provider config for blink.cmp
    Register lazydev as a blink.cmp source provider with module path
    and score_offset=100 to prioritize vim API completions in Lua files.

--- config/nvim/lua/shamindras/plugins/blink.lua
+++ config/nvim/lua/shamindras/plugins/blink.lua
@@ -36,6 +36,13 @@ return {
         per_filetype = {
           lua = { inherit_defaults = true, 'lazydev' },
         },
+        providers = {
+          lazydev = {
+            name = 'LazyDev',
+            module = 'lazydev.integrations.blink',
+            score_offset = 100,
+          },
+        },
       },
 
       completion = {
