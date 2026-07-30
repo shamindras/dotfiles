18c150443d36c3c09496354ba916cb8945cf9032
Shamindra Shrotriya <shamindra.shrotriya@gmail.com>
2026-07-31 02:07:21 +1000

    feat(zsh): add mvb alias for move-books

--- config/zsh/conf.d/11-z1-aliases.zsh
+++ config/zsh/conf.d/11-z1-aliases.zsh
@@ -34,6 +34,7 @@ function z1_aliases {
   alias less='less -R'
   alias md='mkdir -p'
   alias man='batman'
+  alias mvb='$HOME/.config/bin/move-books' # sweep Downloads -> books library + rename epubs
   alias nb='tmux-resize;newsboat;clear;'
   alias paths='echo -e ${PATH//:/\\n}' # Echo all executable Paths
   alias rcp='rsync -ah --info=progress2'
